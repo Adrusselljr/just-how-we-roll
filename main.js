09d2b54029718b7856924c4212c683d880b9ccfc
Aaron Russell Jr <adrusselljr@Aarons-MBP.lan>
2021-11-01 11:36:10 -0400

    modified js

--- main.js
+++ main.js
@@ -407,8 +407,8 @@ const mean = arr => {
 }
 
 const median = arr => {
-  const mid = Math.floor(arr.length / 2),
-    nums = [...arr].sort((a, b) => a - b)
+  const mid = Math.floor(arr.length / 2)
+  const nums = [...arr].sort((a, b) => a - b)
     // console.log(nums)
   return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2
 }
